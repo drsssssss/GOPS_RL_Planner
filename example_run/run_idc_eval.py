a1991e4c2b4958ddef5d335953083ef444ab37cc
张巷腾 <zhzhangxiangteng@qq.com>
2023-12-19 18:24:49 +0800

    add instructions

--- example_run/run_idc_eval.py
+++ example_run/run_idc_eval.py
@@ -3,18 +3,25 @@ import datetime
 import pathlib
 import json
 from gops.trainer.idsim_idc_evaluator import IdsimIDCEvaluator, get_args_from_json
-from idsim.utils.fs import TEMP_ROOT
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 os.environ["OMP_NUM_THREADS"] = "1"
 import torch
 torch.set_num_threads(1)
 
+## How to use this script:
+# 1. Choose scenario_root, ini_network_root, nn_index
+# 2. Modify IDC_MODE. if true, means using IDC to select path, otherwise, only tracking one path
+# 3. Modify PATH_SELECTION_EVIDENCE.
+#       'loss' means using sum of reward to select path,
+#       'value' means using value network to select path.
+# 4. Run this script
+
 if __name__ == "__main__":
-    # ini_network_root = r"D:\Develop\gops-develop\results\pyth_idsim\FHADP2_231212-091300-v12"
-    ini_network_root = r"D:\Develop\gops-develop\results\pyth_idsim\FHADP2_231212-163916-v15"
-    # ini_network_root = r"D:\Develop\gops-develop\results\pyth_idsim\FHADP2_231218-091038"
+    scenario_root = pathlib.Path('YOUR_MAP_ROOT_FOR_TEST')
+    ini_network_root = r"/home/example/results/pyth_idsim/FHADP2_231212-163916"
     nn_index = 20000
+
     config_path = os.path.join(ini_network_root, "config.json")
     args = get_args_from_json(config_path, {})
     args["ini_network_root"] = ini_network_root
@@ -35,7 +42,6 @@ if __name__ == "__main__":
     args['env_config']['max_steps'] = 2000
     args['env_config']['num_scenarios'] = num_scenarios = 4
     args['env_config']['scenario_reuse'] = scenario_reuse = 5
-    scenario_root = pathlib.Path(r'D:\Develop\map\idsim-multilane-v10')
     args['env_config']['scenario_root'] = scenario_root
     args['env_config']['use_logging'] = True
     args['env_config']['singleton_mode'] = 'invalidate' 
--- example_run/run_idsim_video.py
+++ example_run/run_idsim_video.py
@@ -1,5 +1,4 @@
 import json
-import numpy as np
 import pathlib
 import os
 from gops.trainer.idsim_render.animation_crossroad import AnimationCross
@@ -8,52 +7,44 @@ import pickle
 import os
 
 # ------------- How to use --------------
+# 1. Ensure that ffmpeg is installed in your computer and is added to system path
 # 1. Choose Animation=AnimationLane/AnimationCross
 # 2. Edit log_path_root
 # 3. Edit scene_id_list
 # ---------------------------------------
 
-# add ffmpeg path to system path
-current_path = os.environ.get('Path')
-new_path = r'D:\Software\TinySoftware\ffmpeg-2023-12-14-git-5256b2fbe6-full_build\bin'
-os.environ['Path'] = current_path + os.pathsep + new_path
-
 Animation = AnimationLane
 # Animation = AnimationCross
 
-# log_path_root = pathlib.Path("test/230711-172542IDCevaluation")
-# log_path_root = pathlib.Path(r'D:\Develop\gops-develop\results\pyth_idsim\FHADP2_231212-091300-v12\231217-165404IDCevaluation')
-log_path_root = pathlib.Path(r'D:\Develop\gops-develop\results\pyth_idsim\FHADP2_231212-163916-v15\231218-184942IDCevaluation')
-# log_path_root = pathlib.Path(r'D:\Develop\gops-develop\results\pyth_idsim\FHADP2_231218-091038\231218-153832IDCevaluation')
+log_path_root = pathlib.Path(r'/home/example/results/gops_idsim/FHADP2_231212-163916/231218-184942IDCevaluation')
 
-# scene_id_list = ['000', '001', '002', '003']
-scene_id_list = ['000']
-theme_style = 'light'
-# read config.json from log_path_root
-config_path = log_path_root/'config.json'
-with open(config_path, 'r') as f:
-    config = json.load(f)
+scene_id_list = ['000', '001', '002', '003', '004']
+theme_style = 'light' # only works for AnimationCross
 
+if __name__ == '__main__':
+    # read config.json from log_path_root
+    config_path = log_path_root/'config.json'
+    with open(config_path, 'r') as f:
+        config = json.load(f)
 
-for scene_id in scene_id_list:
-    log_path = log_path_root/scene_id
-    save_path = log_path
+    for scene_id in scene_id_list:
+        log_path = log_path_root/scene_id
+        save_path = log_path
 
-    episode_list = [log_path for log_path in os.listdir(
-        log_path) if (log_path.startswith('episode'))]
-    episode_list.sort()
+        episode_list = [log_path for log_path in os.listdir(log_path) if (log_path.startswith('episode'))]
+        episode_list.sort()
 
-    print('episode_list: ', episode_list)
+        print('episode_list: ', episode_list)
 
-    fcd_file_path = log_path/'fcd.xml'
-    print('fcd_file_path: ', fcd_file_path)
+        fcd_file_path = log_path/'fcd.xml'
+        print('fcd_file_path: ', fcd_file_path)
 
-    animation = Animation(theme_style, fcd_file_path, config)
+        animation = Animation(theme_style, fcd_file_path, config)
 
-    for i, episode in enumerate(episode_list):
-        episode_path = log_path / episode
-        with open(episode_path, 'rb') as f:
-            episode_data = pickle.load(f)
-            episode_data.save_folder = log_path_root
+        for i, episode in enumerate(episode_list):
+            episode_path = log_path / episode
+            with open(episode_path, 'rb') as f:
+                episode_data = pickle.load(f)
+                episode_data.save_folder = log_path_root
 
-        animation.generate_animation(episode_data, save_path, i, mode='debug')
+            animation.generate_animation(episode_data, save_path, i, mode='debug')
