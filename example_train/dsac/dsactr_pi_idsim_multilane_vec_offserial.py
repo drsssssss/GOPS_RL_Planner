bcbb6fe65799899bfcec8cc9395e1df1ca957fc1
wwx <17888819590@163.com>
2024-01-14 22:53:01 +0800

    change to four hidden layers

--- example_train/dsac/dsactr_pi_idsim_multilane_vec_offserial.py
+++ example_train/dsac/dsactr_pi_idsim_multilane_vec_offserial.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
         default="TanhGaussDistribution",
         help="Options: default/TanhGaussDistribution/GaussDistribution",
     )
-    parser.add_argument("--policy_hidden_sizes", type=list, default=[256, 256,256])
+    parser.add_argument("--policy_hidden_sizes", type=list, default=[256,256,256,256])
     parser.add_argument(
         "--policy_hidden_activation", type=str, default="gelu", help="Options: relu/gelu/elu/selu/sigmoid/tanh"
     )
@@ -136,7 +136,7 @@ if __name__ == "__main__":
     parser.add_argument("--enable_mask", type=bool, default=True)
     parser.add_argument("--obj_dim", type=int, default=pi_paras["obj_dim"])
     parser.add_argument("--pi_out_dim", type=int, default=pi_paras["output_dim"])
-    parser.add_argument("--pi_hidden_sizes", type=list, default=[256, 256,256])
+    parser.add_argument("--pi_hidden_sizes", type=list, default=[256,256,256,256])
     parser.add_argument("--pi_hidden_activation", type=str, default="gelu")
     parser.add_argument("--pi_output_activation", type=str, default="linear")
     parser.add_argument("--freeze_pi_net", type=str, default="actor")
